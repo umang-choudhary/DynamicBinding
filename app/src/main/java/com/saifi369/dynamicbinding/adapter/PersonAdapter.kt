7b08e20f7c3890810571480490ec3f050c3ec0e8
saifi369 <chsaif369@gmail.com>
2020-07-20 01:32:52 +0500

    RecyclerView DynamicBinding with Data Binding

--- app/src/main/java/com/saifi369/dynamicbinding/adapter/PersonAdapter.kt
+++ app/src/main/java/com/saifi369/dynamicbinding/adapter/PersonAdapter.kt
@@ -1,62 +1,44 @@
 package com.saifi369.dynamicbinding.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.databinding.ViewDataBinding
+import androidx.databinding.library.baseAdapters.BR
 import androidx.recyclerview.widget.RecyclerView
-import com.saifi369.dynamicbinding.R
+import com.saifi369.dynamicbinding.databinding.ItemCallBinding
+import com.saifi369.dynamicbinding.databinding.ItemEmailBinding
+import com.saifi369.dynamicbinding.databinding.ItemMessageBinding
 import com.saifi369.dynamicbinding.model.User
-import kotlinx.android.synthetic.main.item_call.view.*
-import java.lang.IllegalArgumentException
 
 private const val TYPE_CALL = 0
 private const val TYPE_EMAIL = 1
 private const val TYPE_MESSAGE = 2
 
 class PersonAdapter(private val userList: List<User>) :
-    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+    RecyclerView.Adapter<PersonAdapter.BindingViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BindingViewHolder {
 
         val layoutInflater = LayoutInflater.from(parent.context)
-
-        return when(viewType){
+        val binding: ViewDataBinding = when (viewType) {
             TYPE_CALL -> {
-                val rootView = layoutInflater.inflate(R.layout.item_call,parent,false)
-                CallViewHolder(rootView)
+                ItemCallBinding.inflate(layoutInflater, parent, false)
             }
             TYPE_EMAIL -> {
-                val rootView = layoutInflater.inflate(R.layout.item_email,parent,false)
-                EmailViewHolder(rootView)
+                ItemEmailBinding.inflate(layoutInflater, parent, false)
             }
             TYPE_MESSAGE -> {
-                val rootView =  layoutInflater.inflate(R.layout.item_message,parent,false)
-                MessageViewHolder(rootView)
+                ItemMessageBinding.inflate(layoutInflater, parent, false)
             }
             else -> throw IllegalArgumentException("Unknown view type")
         }
+
+        return BindingViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: BindingViewHolder, position: Int) {
         val user = userList.get(position)
-
-        when(getItemViewType(position)){
-            TYPE_CALL->{
-                val callViewHolder = holder as CallViewHolder
-                callViewHolder.tvName.text = user.name
-                callViewHolder.tvPhoneNo.text = user.phoneNo
-            }
-            TYPE_EMAIL->{
-                val emailViewHolder = holder as EmailViewHolder
-                emailViewHolder.tvName.text = user.name
-                emailViewHolder.tvEmail.text = user.email
-            }
-            TYPE_MESSAGE->{
-                val messageViewHolder = holder as MessageViewHolder
-                messageViewHolder.tvName.text = user.name
-                messageViewHolder.tvMessage.text = user.message
-            }
-        }
+        holder.itemview.setVariable(BR.user,user)
+        holder.itemview.executePendingBindings()
     }
 
     override fun getItemCount() = userList.size
@@ -65,18 +47,6 @@ class PersonAdapter(private val userList: List<User>) :
         return userList.get(position).logType.ordinal
     }
 
-    class CallViewHolder(itemview: View) : RecyclerView.ViewHolder(itemview) {
-        val tvName:TextView = itemview.findViewById(R.id.tv_name)
-        val tvPhoneNo:TextView = itemview.findViewById(R.id.tv_phone_no)
-    }
+    class BindingViewHolder(val itemview: ViewDataBinding) : RecyclerView.ViewHolder(itemview.root)
 
-    class EmailViewHolder(itemview: View) : RecyclerView.ViewHolder(itemview) {
-        val tvName:TextView = itemview.findViewById(R.id.tv_name)
-        val tvEmail:TextView = itemview.findViewById(R.id.tv_email)
-    }
-
-    class MessageViewHolder(itemview: View) : RecyclerView.ViewHolder(itemview) {
-        val tvName:TextView = itemview.findViewById(R.id.tv_name)
-        val tvMessage:TextView = itemview.findViewById(R.id.tv_message)
-    }
 }
